92ac58a0164f23ccd1f7053dc0e61a9e7dffae99
Aniket Gupta <ag9326107@gmail.com>
2025-04-30 10:16:47 +0530

    Pass

---
+++ password.py
@@ -0,0 +1,35 @@
+from cryptography.fernet import Fernet
+def write_key():
+    key=Fernet.generate_key()
+    with open('key.key','wb') as keyfile:
+        keyfile.write(key)
+
+def load_key():
+    return open('key.key','rb').read()
+    
+key=load_key()
+cipher=Fernet(key)
+choice=input("Want to Add a New password or view the existing password(view,add): ").lower()
+
+
+def add():
+    username=input("Enter site name or user name: ")
+    password=input("Enter password!: ")
+    encrypted=cipher.encrypt(password.encode())
+    with open('password.txt','a') as f:
+        f.write(username.lower() +"|"+ encrypted.decode()+ "\n")
+def view():
+    username=input("Enter site name or user name: ").lower()
+    with open('password.txt','r') as f:
+        for line in f.readlines():
+            data=line.rstrip()  
+            user,pwd=data.split("|") 
+            if username==user.lower():
+                print("username: "+user+" , Password: "+cipher.decrypt(pwd).decode() )
+if choice=="view":
+    view()
+elif choice=="add":
+    add()
+else:
+    print("invalid attemp!")
+    
