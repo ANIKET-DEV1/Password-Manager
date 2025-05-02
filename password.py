61cdb9dba2520be256670ddd24b9df148464467f
Aniket Gupta <ag9326107@gmail.com>
2025-05-02 12:22:13 +0530

    Password manager

--- password.py
+++ password.py
@@ -6,11 +6,9 @@ def write_key():
 
 def load_key():
     return open('key.key','rb').read()
-    
-key=load_key()
-cipher=Fernet(key)
-choice=input("Want to Add a New password or view the existing password(view,add,Quit): ").lower()
 
+key=load_key()
+cipher=Fernet(key)   
 
 def add():
     username=input("Enter site name or user name: ")
@@ -18,7 +16,9 @@ def add():
     encrypted=cipher.encrypt(password.encode())
     with open('password.txt','a') as f:
         f.write(username.lower() +"|"+ encrypted.decode()+ "\n")
-    print("Data is sucessfully added")
+    print("Password Store Successfully")
+    choice()  
+    
 def view():
     username=input("Enter site name or user name: ").lower()
     with open('password.txt','r') as f:
@@ -26,13 +26,44 @@ def view():
             data=line.rstrip()  
             user,pwd=data.split("|") 
             if username==user.lower():
-                print("username: "+user+" , Password: "+cipher.decrypt(pwd).decode() )
-if choice=="view":
-    view()
-elif choice=="add":
-    add()
-elif choice=="quit":
-    print("Quit")
-else:
-    print("invalid attempt!")
+                print("username: "+user+" , Password: "+cipher.decrypt(pwd.encode()).decode())
+        choice()
+      
+
+def login(a,b):
+    with open('llogin.txt','r') as f:
+        for lines in f.readlines():
+            data=lines.rstrip()
+            user,pwd=data.split("|")
+    if a.lower()==user.lower() and b==pwd:
+        print(f"Login sucessfull {a}")
+        return True
+    else :
+        return False
+def choice():
+        key=load_key()
+        cipher=Fernet(key)
+        choice =input("Want to Add a New password or view the existing password(view,add,quit): ").lower()
+        if choice=="view":
+            view()
+        elif choice=="add":
+            add()
+        elif  choice=="quit":
+            pass
+        else:
+            print("invalid attemp!")
+        
+def main():
+    # write_key()
     
+    print("Welcome to password manager \nTo access the password user must login!")
+    Username=input("Enter username: ")
+    password=input("Enter password: ")
+    f=login(Username,password)
+    if f:
+        choice()
+    else:
+        print("Invalid Credential!")
+  
+
+main()
